124f31a6d2b67a718666ae75aea86000f2f42242
Matheus Mazali Maeda <79715828+matheus-maeda@users.noreply.github.com>
2023-05-31 13:35:19 +0000

    Update ChampionRow.jsx

--- league_champions/src/components/ChampionRow.jsx
+++ league_champions/src/components/ChampionRow.jsx
@@ -10,15 +10,20 @@ export default ({ champions }) => {
   let [actualSkin, setActualSkin] = useState();
   let [actualSkinAux, setActualSkinAux] = useState();
   let [activeSkill, setActiveSkill] = useState();
+  let [canChangeSkin, setCanChangeSkin] = useState(true);
 
   async function fetchChampion(champion) {
     let championSkinsFetch = [];
-    await fetch(`https://ddragon.leagueoflegends.com/cdn/12.15.1/data/pt_BR/champion/${champion}.json`)
+    await fetch(
+      `https://ddragon.leagueoflegends.com/cdn/12.15.1/data/pt_BR/champion/${champion}.json`
+    )
       .then((response) => response.json())
       .then(function (response) {
         setChampionResponse(response.data);
         setChampionName(response.data[champion]["id"]);
-        championSkinsFetch = Object.keys(response.data[champion]["skins"]).map((key) => [response.data[champion]["skins"][key]]);
+        championSkinsFetch = Object.keys(response.data[champion]["skins"]).map(
+          (key) => [response.data[champion]["skins"][key]]
+        );
         setChampionSkins(championSkinsFetch);
       });
     setActualSkin(0);
@@ -29,32 +34,53 @@ export default ({ champions }) => {
 
   function loadSkillInformation(skill) {
     if (skill != "p") {
-      setSkillInformation(championResponse[championName]["spells"][skill]["description"]);
+      setSkillInformation(
+        championResponse[championName]["spells"][skill]["description"]
+      );
       setSkillName(championResponse[championName]["spells"][skill]["name"]);
       setActiveSkill(skill);
     } else {
       setActiveSkill(4);
-      setSkillInformation(championResponse[championName]["passive"]["description"]);
+      setSkillInformation(
+        championResponse[championName]["passive"]["description"]
+      );
       setSkillName(championResponse[championName]["passive"]["name"]);
     }
   }
 
   function changeSkin(args) {
+    setCanChangeSkin(false);
     if (args == "1") {
       if (actualSkinAux < championSkins.length) {
-        setActualSkinAux(actualSkinAux + 1);
-        setActualSkin(championSkins[actualSkinAux][0]["num"]);
+        try {
+          setActualSkinAux(actualSkinAux + 1);
+          setActualSkin(championSkins[actualSkinAux][0]["num"]);
+        } finally {
+          setCanChangeSkin(true);
+        }
       } else {
-        setActualSkinAux(0);
-        setActualSkin(0);
+        try {
+          setActualSkinAux(0);
+          setActualSkin(0);
+        } finally {
+          setCanChangeSkin(true);
+        }
       }
     } else {
       if (actualSkin > 0) {
-        setActualSkinAux(actualSkinAux - 1);
-        setActualSkin(championSkins[actualSkinAux][0]["num"]);
+        try {
+          setActualSkinAux(actualSkinAux - 1);
+          setActualSkin(championSkins[actualSkinAux][0]["num"]);
+        } finally {
+          setCanChangeSkin(true);
+        }
       } else {
-        setActualSkinAux(championSkins.length - 1);
-        setActualSkin(championSkins[championSkins.length - 1][0]["num"]);
+        try {
+          setActualSkinAux(championSkins.length - 1);
+          setActualSkin(championSkins[championSkins.length - 1][0]["num"]);
+        } finally {
+          setCanChangeSkin(true);
+        }
       }
     }
   }
@@ -63,8 +89,17 @@ export default ({ champions }) => {
     <div className="Container">
       <div className="ChampionRow">
         {champions.map((element) => (
-          <div className="champion" key={element[0].id} onClick={() => fetchChampion(element[0].id)}>
-            <img src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${element[0].image.full}`} style={{ transform: championName == element[0].id ? "scale(1.2)" : "" }} />
+          <div
+            className="champion"
+            key={element[0].id}
+            onClick={() => fetchChampion(element[0].id)}
+          >
+            <img
+              src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${element[0].image.full}`}
+              style={{
+                transform: championName == element[0].id ? "scale(1.2)" : "",
+              }}
+            />
             <p>{element[0].name}</p>
           </div>
         ))}
@@ -72,37 +107,66 @@ export default ({ champions }) => {
       {championName != "" ? (
         <div className="ChampionInfo">
           <div className="championImage">
-            <button onClick={() => changeSkin(0)}>{"<"}</button>
-            <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championName}_${actualSkin}.jpg`} />
+            <button onClick={() => changeSkin(0)} style={{visible: canChangeSkin ? "visible" : "none"}}>{"<"}</button>
+            <img
+              src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championName}_${actualSkin}.jpg`}
+            />
             <button onClick={() => changeSkin("1")}>{">"}</button>
           </div>
-          <div className="ChampionName">{actualSkin === 0 ? <h2>{championResponse[championName]["name"]}</h2> : <h2>{championResponse[championName]["skins"][actualSkinAux - 1]["name"]}</h2>}</div>
+          <div className="ChampionName">
+            {actualSkin === 0 ? (
+              <h2>{championResponse[championName]["name"]}</h2>
+            ) : (
+              <h2>
+                {
+                  championResponse[championName]["skins"][actualSkinAux - 1][
+                    "name"
+                  ]
+                }
+              </h2>
+            )}
+          </div>
           <div className="ChampionSkills">
-            <div className="ChampionSkillsP" onClick={() => loadSkillInformation("p")}>
+            <div
+              className="ChampionSkillsP"
+              onClick={() => loadSkillInformation("p")}
+            >
               <img
                 src={`https://ddragon.leagueoflegends.com/cdn/12.15.1/img/passive/${championResponse[championName]["passive"]["image"]["full"]}`}
                 style={{ transform: activeSkill == 4 ? "scale(1.1)" : "none" }}
               />
             </div>
-            <div className="ChampionSkillsQ" onClick={() => loadSkillInformation("0")}>
+            <div
+              className="ChampionSkillsQ"
+              onClick={() => loadSkillInformation("0")}
+            >
               <img
                 src={`https://ddragon.leagueoflegends.com/cdn/12.15.1/img/spell/${championResponse[championName]["spells"]["0"]["id"]}.png`}
                 style={{ transform: activeSkill == 0 ? "scale(1.1)" : "none" }}
               />
             </div>
-            <div className="ChampionSkillsW" onClick={() => loadSkillInformation("1")}>
+            <div
+              className="ChampionSkillsW"
+              onClick={() => loadSkillInformation("1")}
+            >
               <img
                 src={`https://ddragon.leagueoflegends.com/cdn/12.15.1/img/spell/${championResponse[championName]["spells"]["1"]["id"]}.png`}
                 style={{ transform: activeSkill == 1 ? "scale(1.1)" : "none" }}
               />
             </div>
-            <div className="ChampionSkillsE" onClick={() => loadSkillInformation("2")}>
+            <div
+              className="ChampionSkillsE"
+              onClick={() => loadSkillInformation("2")}
+            >
               <img
                 src={`https://ddragon.leagueoflegends.com/cdn/12.15.1/img/spell/${championResponse[championName]["spells"]["2"]["id"]}.png`}
                 style={{ transform: activeSkill == 2 ? "scale(1.1)" : "none" }}
               />
             </div>
-            <div className="ChampionSkillsR" onClick={() => loadSkillInformation("3")}>
+            <div
+              className="ChampionSkillsR"
+              onClick={() => loadSkillInformation("3")}
+            >
               <img
                 src={`https://ddragon.leagueoflegends.com/cdn/12.15.1/img/spell/${championResponse[championName]["spells"]["3"]["id"]}.png`}
                 style={{ transform: activeSkill == 3 ? "scale(1.1)" : "none" }}
