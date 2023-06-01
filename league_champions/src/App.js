017f5f0e55dba0ac0ede6bd4731ff131c1c8237c
Matheus Mazali Maeda <79715828+matheus-maeda@users.noreply.github.com>
2023-06-01 15:47:43 -0300

    commit

--- league_champions/src/App.js
+++ league_champions/src/App.js
@@ -1,25 +1,30 @@
-import React, {useState, useEffect} from 'react';
-import ChampionRow from './components/ChampionRow.jsx'
-import Header from "./components/Header.jsx"
-import './App.css'
+import React, { useState, useEffect } from "react";
+import ChampionRow from "./components/ChampionRow.jsx";
+import Header from "./components/Header.jsx";
+import "./App.css";
 export default () => {
+  const [getChampions, setChampions] = useState([]);
 
-  const [getChampions, setChampions] = useState([])
-  
   useEffect(() => {
-        let champions = []
-        fetch('https://ddragon.leagueoflegends.com/cdn/12.15.1/data/pt_BR/champion.json').then(response => response.json()).then(function(response) {
-            champions = Object.keys(response.data).map((key) => [response.data[key]]);
-            setChampions(champions)
-        })
-  },[])
+    let champions = [];
+    fetch(
+      "http://ddragon.leagueoflegends.com/cdn/13.10.1/data/pt_BR/champion.json"
+    )
+      .then((response) => response.json())
+      .then(function (response) {
+        champions = Object.keys(response.data).map((key) => [
+          response.data[key],
+        ]);
+        setChampions(champions);
+      });
+  }, []);
 
   return (
-    <div className='App'>
-      <div className='fullContainer'>
+    <div className="App">
+      <div className="fullContainer">
         <Header />
         <ChampionRow champions={getChampions} />
       </div>
     </div>
   );
-}
+};
